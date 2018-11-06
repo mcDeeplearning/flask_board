e174e8f7f91f926efaf318b00c844cc99738468f
changhee <change@hphk.kr>
2018-11-06 02:04:18 +0000

    create 기본로직작성

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -13,4 +13,20 @@ migrate = Migrate(app,db)
 
 @app.route('/')
 def index():
-    return "hello"
+    return render_template('index.html')
+
+@app.route('/posts/new')
+def new():
+    return render_template('new.html')
+
+@app.route('/posts/create')
+def create():
+    title = request.args.get('title')
+    content = request.args.get('content')
+    post = Post(title=title, content=content)
+    db.session.add(post)
+    db.session.commit()
+    
+    return render_template('create.html')
+    
+    
