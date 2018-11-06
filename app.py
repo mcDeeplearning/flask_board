b94739448e7b88ffa93e548774b4e5b1b35b3d90
changhee <change@hphk.kr>
2018-11-06 06:47:13 +0000

    post방식

--- app.py
+++ app.py
@@ -23,15 +23,18 @@ def index():
 def new():
     return render_template('new.html')
 
-@app.route('/posts/create')
+@app.route('/posts/create', methods=["POST"])
 def create():
-    title = request.args.get('title')
-    content = request.args.get('content')
+    # title = request.args.get('title')
+    title = request.form.get('title')
+    # content = request.args.get('content')
+    content = request.form.get('content')
     post = Post(title=title, content=content)
     db.session.add(post)
     db.session.commit()
     
-    return render_template('create.html',post=post)
+    # return render_template('create.html')
+    return redirect('/posts/{}'.format(post.id))
     
 @app.route('/posts/<int:id>')
 def read(id):
@@ -48,3 +51,18 @@ def delete(id):
     
     return redirect('/')
     
+@app.route('/posts/<int:id>/edit')
+def edit(id):
+    post = Post.query.get(id)
+    return render_template('edit.html',post=post)
+    
+@app.route('/posts/<int:id>/update', methods=["POST"])
+def update(id):
+    post = Post.query.get(id)
+    post.title = request.form.get('title')
+    post.content = request.form.get('content')
+    # post.title = request.args.get('title')
+    # post.content = request.args.get('content')
+    db.session.commit()
+    
+    return redirect('/posts/{}'.format(id))
