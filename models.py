07d8ab3aff88e9d3ebbf7f09a4b773b343129350
changhee <change@hphk.kr>
2018-11-09 00:40:19 +0000

    model fix

--- models.py
+++ models.py
@@ -10,9 +10,27 @@ class Post(db.Model):
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.Text)
     created_at = db.Column(db.DateTime)
+    comments = db.relationship('Comment',backref='post')
     
     # 생성자
     def __init__(self,title,content):
         self.title = title
         self.content = content
         self.created_at = datetime.datetime.now()
+        
+class Comment(db.Model):
+    __tablename__ ='comments'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))  
+    
+    def __init__(self,content):
+        self.content = content
+
+
+
+
+
+
+
+
